8286858a99ed2c20a19bec3d5339dd347a47b529
Owen Simpson <owensimpson1127@gmail.com>
2023-10-20 14:58:14 -0400

    update

--- src/routes/pgcr.ts
+++ src/routes/pgcr.ts
@@ -1,20 +1,24 @@
-import express, {Request, Response} from "express"
-import {success} from "../util"
-import {prisma} from "../database"
+import express, { Request, Response } from "express"
+import { failure, success } from "../util"
+import { prisma } from "../database"
 
 export const pgcrRouter = express.Router()
 
 pgcrRouter.get("/:activityId", async (req: Request, res: Response) => {
     const activityId = req.params.activityId
 
-    const data = await getActivity(activityId)
-    return res.status(200).json(success(data))
+    try {
+        const data = await getActivity(activityId)
+        return res.status(200).json(success(data))
+    } catch (e) {
+        res.status(500).json(failure(e, "Internal server error"))
+    }
 })
 
 async function getActivity(activityId: string) {
-    return await prisma.activity.findUniqueOrThrow({
+    return prisma.rawPGCR.findUniqueOrThrow({
         where: {
-            activityId: activityId
+            id: activityId
         }
     })
 }
