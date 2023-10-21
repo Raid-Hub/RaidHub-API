abe92a54ef1cc8cffc62b3251074ea79859f9db3
Owen Simpson <owensimpson1127@gmail.com>
2023-10-20 23:11:08 -0400

    updates

--- src/routes/activities.ts
+++ src/routes/activities.ts
@@ -1,22 +1,31 @@
 import express, { Request, Response } from "express"
 import { failure, success } from "../util"
 import { prisma } from "../database"
+import { NextFunction } from "connect"
 
 export const activitiesRouter = express.Router()
 
+const cacheCursoredReqsMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    if (req.query.cursor) {
+        // Set cache headers to last for 24 hours (in seconds)
+        res.setHeader("Cache-Control", "max-age=86400")
+    }
+
+    next()
+}
+
+activitiesRouter.use(cacheCursoredReqsMiddleware)
+
 activitiesRouter.get("/:destinyMembershipId", async (req: Request, res: Response) => {
     const membershipId = req.params.destinyMembershipId
-    let page: number | undefined = Number(req.query.page)
-    if (Number.isNaN(page)) {
-        page = undefined
-    }
+    const cursor = req.query.cursor ? String(req.query.cursor) : null
     let count: number | undefined = Number(req.query.count)
     if (Number.isNaN(count)) {
         count = undefined
     }
-
+    console.log(cursor)
     try {
-        const data = await getPlayerActivities({ membershipId, page, count })
+        const data = await getPlayerActivities({ membershipId, cursor, count })
         res.status(200).json(success(data))
     } catch (e) {
         res.status(500).json(failure(e, "Internal server error"))
@@ -25,77 +34,111 @@ activitiesRouter.get("/:destinyMembershipId", async (req: Request, res: Response
 
 async function getPlayerActivities({
     membershipId,
-    page,
+    cursor,
     count
 }: {
     membershipId: string
-    page?: number
+    cursor: string | null
     count?: number
 }) {
     count = count ?? 250
-    page = page ?? 1
-    const [activities, playerActivities] = await Promise.all([
-        prisma.activity.findMany({
-            where: {
-                playerActivities: {
-                    some: {
-                        membershipId: membershipId
-                    }
-                }
-            },
-            take: count + 1,
-            skip: (page - 1) * count,
-            orderBy: {
-                dateCompleted: "desc"
-            }
-        }),
-        prisma.playerActivities.findMany({
-            where: {
-                membershipId: membershipId
-            },
-            take: count + 1,
-            skip: (page - 1) * count,
-            select: {
-                finishedRaid: true
-            },
-            orderBy: {
-                activity: {
-                    dateCompleted: "desc"
-                }
-            }
-        })
-    ])
+    /* This allows us to fetch the same set of activities for the first request each day, making caching just a bit better. We
+    can cache subsequent pages, while leaving the first one open */
+    const today = new Date()
+    today.setUTCHours(0, 0, 0, 0)
+    const lastMonth = new Date(today)
+    lastMonth.setMonth(today.getUTCMonth() - 1)
+
+    const [activities, playerActivities] = await Promise.all(
+        // If a cursor is provided
+        cursor
+            ? [
+                  prisma.activity.findMany({
+                      where: {
+                          playerActivities: {
+                              some: {
+                                  membershipId: membershipId
+                              }
+                          }
+                      },
+                      take: count + 1,
+                      cursor: {
+                          activityId: cursor
+                      },
+                      orderBy: {
+                          dateCompleted: "desc"
+                      }
+                  }),
+                  prisma.playerActivities.findMany({
+                      where: {
+                          membershipId: membershipId
+                      },
+                      take: count + 1,
+                      cursor: {
+                          activity_player_index: {
+                              activityId: cursor,
+                              membershipId: membershipId
+                          }
+                      },
+                      select: {
+                          finishedRaid: true
+                      },
+                      orderBy: {
+                          activity: {
+                              dateCompleted: "desc"
+                          }
+                      }
+                  })
+              ]
+            : [
+                  // If no cursor is provided
+                  prisma.activity.findMany({
+                      where: {
+                          playerActivities: {
+                              some: {
+                                  membershipId: membershipId
+                              }
+                          },
+                          dateCompleted: {
+                              gte: lastMonth,
+                              lte: today
+                          }
+                      },
+                      take: count + 1,
+                      orderBy: {
+                          dateCompleted: "desc"
+                      }
+                  }),
+                  prisma.playerActivities.findMany({
+                      where: {
+                          membershipId: membershipId,
+                          activity: {
+                              dateCompleted: {
+                                  gte: lastMonth,
+                                  lte: today
+                              }
+                          }
+                      },
+                      take: count + 1,
+                      select: {
+                          finishedRaid: true
+                      },
+                      orderBy: {
+                          activity: {
+                              dateCompleted: "desc"
+                          }
+                      }
+                  })
+              ]
+    )
 
     return {
-        hasMore: !!activities[count],
+        prevActivity: cursor
+            ? activities[count]?.activityId ?? null
+            : activities[activities.length - 1]!.activityId,
         activities: activities.slice(0, count).map((a, i) => ({
             ...a,
             didMemberComplete: playerActivities[i].finishedRaid
         }))
     }
 }
-
-// include: {
-//     playerActivities: {
-//         select: {
-//             finishedRaid: true,
-//             player: {
-//                 select: {
-//                     membershipId: true
-//                 }
-//             }
-//         }
-//     }
-// }
-
-// activities: Object.fromEntries(
-//     activities.slice(0, count).map(({ playerActivities, ...activity }) => [
-//         activity.activityId,
-//         {
-//             ...activity,
-//             players: Object.fromEntries(
-//                 playerActivities.map(ap => [ap.player.membershipId, ap.finishedRaid])
-//             )
-//         }
-//     ])
-// )
