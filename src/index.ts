c77009e43a0bad91428795364545851ed929e270
Owen <98496129+owensimpson@users.noreply.github.com>
2023-11-01 17:21:04 -0400

    add sf vog endpoint (#13)

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { activityRouter } from "./routes/activity"
 import { leaderboardRouter } from "./routes/leaderboard"
 import { playerRouter } from "./routes/player"
 import { searchRouter } from "./routes/search"
+import { sfRouter } from "./routes/soloflawless"
 
 dotenv.config()
 
@@ -33,6 +34,7 @@ app.use("/manifest", manifestRouter)
 app.use("/leaderboard", leaderboardRouter)
 app.use("/player", playerRouter)
 app.use("/search", searchRouter)
+app.use("/soloflawlessvog", sfRouter)
 
 app.listen(port, () => {
     console.log("Express server started on port: " + port)
