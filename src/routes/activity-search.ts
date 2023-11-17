0874b7385be15a6bb77ef4cf2139529c23915fce
owensimpson <owensimpson1127@gmail.com>
2023-11-16 20:55:30 -0500

    minor changes

--- src/routes/activity-search.ts
+++ src/routes/activity-search.ts
@@ -5,25 +5,31 @@ import { zodQueryParser } from "~/middlewares/parsers"
 import { z } from "zod"
 import { type ListedRaid, ListedRaids, RaidHashes } from "~/data/raids"
 import { SeasonDates } from "~/data/seasonDates"
+import { cacheControl } from "~/middlewares/cache-control"
 
 export const activitySearchRouter = Router()
+
+activitySearchRouter.use(cacheControl(30))
 
 const activitySearchQuerySchema = z
     .object({
         membershipId: z.union([z.array(numberString).min(1), numberString.transform(s => [s])]),
-        minPlayers: z.coerce.number().int().nonnegative().default(1),
-        maxPlayers: z.coerce.number().int().nonnegative().default(16384),
+        minPlayers: z.coerce.number().int().nonnegative().optional(),
+        maxPlayers: z.coerce.number().int().nonnegative().optional(),
         minDate: z.coerce.date().optional(),
         maxDate: z.coerce.date().optional(),
-        minSeason: z.coerce.number().int().nonnegative().default(1),
-        maxSeason: z.coerce.number().int().nonnegative().default(50),
+        minSeason: z.coerce.number().int().nonnegative().optional(),
+        maxSeason: z.coerce.number().int().nonnegative().optional(),
         fresh: booleanString.optional(),
         completed: booleanString.optional(),
         flawless: booleanString.optional(),
         raid: z.coerce
             .number()
             .int()
-            .refine(n => includedIn(ListedRaids, n))
+            .positive()
+            .refine(n => includedIn(ListedRaids, n), {
+                message: "invalid raid value"
+            })
             .optional(),
         platformType: z.coerce.number().int().positive().optional(),
         reversed: z.coerce.boolean().default(false),
@@ -38,14 +44,29 @@ const activitySearchQuerySchema = z
     }))
 
 // Todo: add a query param for the difficulty
-activitySearchRouter.get("/", zodQueryParser(activitySearchQuerySchema), async (req, res) => {
-    const activities = await searchActivities(req.query)
-    res.status(200).json(
-        success({
-            query: req.query,
-            results: activities
-        })
-    )
+activitySearchRouter.get("/", zodQueryParser(activitySearchQuerySchema), async (req, res, next) => {
+    try {
+        const activities = await searchActivities(req.query)
+        const results = activities.map(a => ({
+            instanceId: a.instance_id,
+            raidHash: a.raid_hash,
+            false: a.fresh,
+            completed: a.completed,
+            flawless: a.flawless,
+            playerCount: a.player_count,
+            dateStarted: a.date_started,
+            dateCompleted: a.date_completed,
+            platformType: a.platform_type
+        }))
+        res.status(200).json(
+            success({
+                query: req.query,
+                results
+            })
+        )
+    } catch (e) {
+        next(e)
+    }
 })
 
 type ActivitySearchResult = {
@@ -79,9 +100,11 @@ async function searchActivities({
 }: z.infer<typeof activitySearchQuerySchema>) {
     // @ts-ignore
     const hashes = RaidHashes[raid] ? (Object.values(RaidHashes[raid]).flat() as string[]) : []
-    const minSeasonDate = SeasonDates[minSeason] ?? SeasonDates[0]
+    const minSeasonDate = minSeason ? SeasonDates[minSeason] ?? SeasonDates[0] : SeasonDates[0]
     // do plus once because the season dates are the start dates
-    const maxSeasonDate = SeasonDates[maxSeason + 1] ?? new Date(2000000000000)
+    const maxSeasonDate = maxSeason
+        ? SeasonDates[maxSeason + 1] ?? new Date(2000000000000)
+        : new Date(2000000000000)
 
     const results = await prisma.$queryRaw<Array<ActivitySearchResult>>`
         WITH activities_union AS (
@@ -99,7 +122,7 @@ async function searchActivities({
                 (${flawless}::boolean IS NULL OR a.flawless = ${flawless}) AND   
                 (${hashes.length} = 0 OR a.raid_hash = ANY(${hashes}::bigint[])) AND
                 (${platformType}::int IS NULL OR a.platform_type = ${platformType}) AND
-                a.player_count BETWEEN ${minPlayers} AND ${maxPlayers} AND
+                a.player_count BETWEEN ${minPlayers ?? 1} AND ${maxPlayers ?? 16384} AND
                 a.date_completed BETWEEN ${minSeasonDate} AND ${maxSeasonDate} AND
                 a.date_completed BETWEEN ${minDate ?? SeasonDates[0]} AND ${maxDate ?? new Date()}
             GROUP BY
