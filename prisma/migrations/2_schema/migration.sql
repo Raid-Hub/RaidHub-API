8be728a247335f10bbd8171f2bdb51f30d3cd39c
Owen <98496129+owensimpson@users.noreply.github.com>
2024-01-07 22:25:02 -0500

    Sherpas and clears (#23)
    * update schema
    
    * update schema and seed script
    
    * create router
    
    * change sherpa count

--- prisma/migrations/2_schema/migration.sql
+++ prisma/migrations/2_schema/migration.sql
@@ -37,15 +37,21 @@ CREATE TABLE "player_activity" (
     "deaths" INTEGER NOT NULL DEFAULT 0,
     "time_played_seconds" INTEGER NOT NULL DEFAULT 0,
     "class_hash" BIGINT,
+    "sherpas" INTEGER NOT NULL DEFAULT 0,
 
     CONSTRAINT "player_activity_instance_id_membership_id_pkey" PRIMARY KEY ("instance_id","membership_id")
 );
 
 -- CreateTable
-CREATE TABLE "clear_count" (
+CREATE TABLE "player_stats" (
     "membership_id" BIGINT NOT NULL,
-    "count" INTEGER NOT NULL,
-    "raid_id" INTEGER NOT NULL
+    "raid_id" INTEGER NOT NULL,
+    "clears" INTEGER NOT NULL DEFAULT 0,
+    "fresh_clears" INTEGER NOT NULL DEFAULT 0,
+    "sherpas" INTEGER NOT NULL DEFAULT 0,
+    "trios" INTEGER NOT NULL DEFAULT 0,
+    "duos" INTEGER NOT NULL DEFAULT 0,
+    "solos" INTEGER NOT NULL DEFAULT 0
 );
 
 -- CreateTable
@@ -129,7 +135,15 @@ CREATE INDEX "idx_instance_id" ON "player_activity"("instance_id");
 CREATE INDEX "idx_membership_id" ON "player_activity"("membership_id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "clear_count_membership_id_raid_id_key" ON "clear_count"("membership_id", "raid_id");
+CREATE UNIQUE INDEX "player_stats_membership_id_raid_id_key" ON "player_stats"("membership_id", "raid_id");
+
+-- CreateIndex
+CREATE INDEX "raid_clears_idx" ON "player_stats"("raid_id", "clears" DESC);
+CREATE INDEX "raid_fresh_clears_idx" ON "player_stats"("raid_id", "fresh_clears" DESC);
+CREATE INDEX "raid_sherpas_idx" ON "player_stats"("raid_id", "sherpas" DESC);
+CREATE INDEX "raid_trio_clears_idx" ON "player_stats"("raid_id", "trios" DESC);
+CREATE INDEX "raid_duo_clears_idx" ON "player_stats"("raid_id", "duos" DESC);
+CREATE INDEX "raid_solo_clears_idx" ON "player_stats"("raid_id", "solos" DESC);
 
 -- CreateIndex
 CREATE UNIQUE INDEX "leaderboard_id_key" ON "leaderboard"("id");
@@ -153,10 +167,10 @@ ALTER TABLE "player_activity" ADD CONSTRAINT "player_activity_instance_id_fkey" 
 ALTER TABLE "player_activity" ADD CONSTRAINT "player_activity_membership_id_fkey" FOREIGN KEY ("membership_id") REFERENCES "player"("membership_id") ON DELETE RESTRICT ON UPDATE NO ACTION;
 
 -- AddForeignKey
-ALTER TABLE "clear_count" ADD CONSTRAINT "raid_id_fkey" FOREIGN KEY ("raid_id") REFERENCES "raid"("id") ON DELETE RESTRICT ON UPDATE NO ACTION;
+ALTER TABLE "player_stats" ADD CONSTRAINT "raid_id_fkey" FOREIGN KEY ("raid_id") REFERENCES "raid"("id") ON DELETE RESTRICT ON UPDATE NO ACTION;
 
 -- AddForeignKey
-ALTER TABLE "clear_count" ADD CONSTRAINT "player_membership_id_fkey" FOREIGN KEY ("membership_id") REFERENCES "player"("membership_id") ON DELETE CASCADE ON UPDATE NO ACTION;
+ALTER TABLE "player_stats" ADD CONSTRAINT "player_membership_id_fkey" FOREIGN KEY ("membership_id") REFERENCES "player"("membership_id") ON DELETE RESTRICT ON UPDATE NO ACTION;
 
 -- AddForeignKey
 ALTER TABLE "activity_leaderboard_entry" ADD CONSTRAINT "activity_leaderboard_entry_instance_id_fkey" FOREIGN KEY ("instance_id") REFERENCES "activity"("instance_id") ON DELETE NO ACTION ON UPDATE NO ACTION;
