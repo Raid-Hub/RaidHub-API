68325adeebcd98a52f3144232fdd596451058672
Owen <98496129+owens1127@users.noreply.github.com>
2024-01-27 13:01:58 -0500

    testing123 (#31)
    * testing123
    
    * rename file
    
    * perms
    
    * +x
    
    * fix coverage format
    
    * grant more perms for finding coverage
    
    * diff action
    
    * change workflow name param
    
    * dont require main branch
    
    * test
    
    * test without a name
    
    * file idk
    
    * with name
    
    * update checkout action (v3 -> v4)

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-    coverageReporters: ["lcov"],
     preset: "ts-jest",
     testTimeout: 15000,
     testEnvironment: "node",
