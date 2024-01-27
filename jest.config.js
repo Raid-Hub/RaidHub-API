1c528d79b6d2dc070e52f8b0faebcc7c43611cd5
owensimpson <owensimpson1127@gmail.com>
2024-01-26 23:55:53 -0500

    test new code cov

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-    coverageReporters: ["html"],
+    coverageReporters: ["lcov"],
     preset: "ts-jest",
     testEnvironment: "node",
     modulePathIgnorePatterns: ["dist/*"],
