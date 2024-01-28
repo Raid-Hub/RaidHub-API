7bd471ae787e8434789c44b87bc98302c715d074
owensimpson <owensimpson1127@gmail.com>
2024-01-28 16:45:12 -0500

    update config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
     preset: "ts-jest",
-    testTimeout: 15000,
     testEnvironment: "node",
     modulePathIgnorePatterns: ["dist/*"],
     setupFiles: ["dotenv/config"],
