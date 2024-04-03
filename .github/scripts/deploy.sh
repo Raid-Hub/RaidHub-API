171520e0d62546d09c6d60738b069f70716395e7
Owen Simpson <owensimpson1127@gmail.com>
2024-04-02 22:40:56 -0400

    fix deploy script

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,13 +1,19 @@
+source ~/.nvm/nvm.sh
+export BUN_INSTALL="$HOME/.bun"
+export PATH=$BUN_INSTALL/bin:$PATH
+
+nvm use 20
+
 git fetch
 git stash
-git reset --hard origin/$1 
+git checkout origin/$1 
 
+bun install --frozen-lockfile
 bun prisma generate
 bun compile $2
 
 sudo systemctl restart $2
 
 if [[ ! -z $3 ]]  ; then 
-    git checkout - 
-    git stash apply
+    git switch -
 fi
