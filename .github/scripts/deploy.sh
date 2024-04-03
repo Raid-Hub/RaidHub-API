e4f5e418a5789b23f882d99dbbe253dc7185a7c9
Owen Simpson <owensimpson1127@gmail.com>
2024-04-02 21:45:25 -0400

    minor fixes

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,2 +1,13 @@
-bun rebuild $1
-sudo systemctl restart $1
+git fetch
+git stash
+git reset --hard origin/$1 
+
+bun prisma generate
+bun compile $2
+
+sudo systemctl restart $2
+
+if [[ ! -z $3 ]]  ; then 
+    git checkout - 
+    git stash apply
+fi
