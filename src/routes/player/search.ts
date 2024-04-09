b2ccc4a53bd9e3f00fe8b914cb19a8e03be67513
Owen <98496129+owens1127@users.noreply.github.com>
2024-04-08 23:43:10 -0400

    improve search (#47)
    * improve search
    
    * test timeout

--- src/routes/player/search.ts
+++ src/routes/player/search.ts
@@ -1,4 +1,4 @@
-import { Player } from "@prisma/client"
+import { Prisma } from "@prisma/client"
 import { RaidHubRoute } from "../../RaidHubRoute"
 import { cacheControl } from "../../middlewares/cache-control"
 import { zPlayerInfo } from "../../schema/common"
@@ -15,8 +15,11 @@ export const playerSearchRoute = new RaidHubRoute({
     middlewares: [cacheControl(60)],
     async handler(req) {
         const { query, count } = req.query
-        const data = await searchForPlayer(query, count)
-        return ok(data)
+        const { searchTerm, results } = await searchForPlayer(query, count)
+        return ok({
+            params: { count, query: searchTerm },
+            results
+        })
     },
     response: {
         success: {
@@ -25,10 +28,7 @@ export const playerSearchRoute = new RaidHubRoute({
                 .object({
                     params: z.object({
                         count: zPositiveInt(),
-                        term: z.object({
-                            name: z.string(),
-                            nameWithCode: z.string().nullable()
-                        })
+                        query: z.string()
                     }),
                     results: z.array(zPlayerInfo.extend({ clears: z.number().int().nonnegative() }))
                 })
@@ -39,132 +39,53 @@ export const playerSearchRoute = new RaidHubRoute({
 
 async function searchForPlayer(query: string, count: number) {
     const searchTerm = query.trim().toLowerCase()
-
-    const select = {
-        bungieGlobalDisplayName: true,
-        bungieGlobalDisplayNameCode: true,
-        lastSeen: true,
-        displayName: true,
-        membershipId: true,
-        iconPath: true,
-        membershipType: true,
-        clears: true
-    } as const
-
-    // normalize last played by adding a month minimum
-    const adjustedNow = Date.now() + 1000 * 60 * 60 * 24 * 30
-    const sortResults = (
-        a: Pick<Player, keyof typeof select>,
-        b: Pick<Player, keyof typeof select>
-    ) => {
-        const aMatch = a.bungieGlobalDisplayName?.toLowerCase() === searchTerm
-        const bMatch = b.bungieGlobalDisplayName?.toLowerCase() === searchTerm
-
-        // @ts-expect-error ts does not like bitwise operators on booleans
-        if (aMatch ^ bMatch) {
-            return aMatch ? -1 : 1
-        } else {
-            const timeDifferenceA = adjustedNow - a.lastSeen.getTime()
-            const timeDifferenceB = adjustedNow - b.lastSeen.getTime()
-            return timeDifferenceA / (a.clears || 1) - timeDifferenceB / (b.clears || 1)
-        }
+    let results = await search(searchTerm, count)
+    if (!results.length) {
+        results = await search(searchTerm, count, true)
     }
-    async function searchGlobal() {
-        const [globalDisplayName, globalDisplayNameCode] = searchTerm.split("#")
-        const results = await prisma.player
-            .findMany({
-                select: select,
-                where: {
-                    bungieGlobalDisplayName: {
-                        equals: globalDisplayName,
-                        mode: "insensitive"
-                    },
-                    bungieGlobalDisplayNameCode: {
-                        startsWith: globalDisplayNameCode.slice(0, 4)
-                    },
-                    lastSeen: {
-                        gt: new Date(0)
-                    }
-                },
-                take: count
-            })
-            .then(res => res.sort(sortResults))
-
-        return {
-            params: {
-                count: count,
-                term: {
-                    name: globalDisplayName,
-                    nameWithCode: globalDisplayName + "#" + globalDisplayNameCode.slice(0, 4)
-                }
-            },
-            results: results.map(r => ({
-                ...r,
-                membershipId: String(r.membershipId)
-            }))
-        }
+    return {
+        searchTerm,
+        results
     }
+}
 
-    async function searchDisplay() {
-        const take = count * 2
-        const results = await prisma.player
-            .findMany({
-                select: select,
-                where: {
-                    bungieGlobalDisplayName: {
-                        startsWith: searchTerm,
-                        mode: "insensitive"
-                    },
-                    lastSeen: {
-                        gt: new Date(0)
-                    }
-                },
-                orderBy: {
-                    clears: "desc"
-                },
-                take: take
-            })
-            .then(res => res.sort(sortResults))
+const searchDisplay = Prisma.sql`WHERE lower(display_name)`
+const searchGlobal = Prisma.sql`WHERE lower(bungie_name)`
+async function search(searchTerm: string, count: number, byDisplayName: boolean = false) {
+    const results = await prisma.$queryRaw<
+        {
+            membership_id: string
+            membership_type: number
+            bungie_global_display_name: string
+            bungie_global_display_name_code: string
+            display_name: string
+            last_seen: Date
+            icon_path: string
+            clears: number
+        }[]
+    >`SELECT 
+        membership_id,
+        membership_type,
+        bungie_global_display_name,
+        bungie_global_display_name_code,
+        display_name,
+        last_seen,
+        icon_path,
+        clears
+    FROM player 
+    ${byDisplayName ? searchDisplay : searchGlobal} LIKE ${searchTerm + "%"} 
+        AND last_seen > TIMESTAMP 'epoch'
+    ORDER BY _search_score DESC 
+    LIMIT ${count};`
 
-        if (results.length < count) {
-            const more = await prisma.player
-                .findMany({
-                    where: {
-                        displayName: {
-                            contains: searchTerm,
-                            mode: "insensitive"
-                        },
-                        NOT: {
-                            bungieGlobalDisplayName: {
-                                startsWith: searchTerm,
-                                mode: "insensitive"
-                            }
-                        }
-                    },
-                    orderBy: {
-                        clears: "desc"
-                    },
-                    take: take - results.length
-                })
-                .then(res => res.sort(sortResults))
-            results.push(...more)
-        }
-
-        return {
-            params: {
-                count: take,
-                term: {
-                    name: searchTerm,
-                    nameWithCode: null
-                }
-            },
-            // sort by a combination of last played and clears
-            results: results.slice(0, count).map(r => ({
-                ...r,
-                membershipId: String(r.membershipId)
-            }))
-        }
-    }
-
-    return searchTerm.includes("#") ? searchGlobal() : searchDisplay()
+    return results.map(r => ({
+        membershipId: r.membership_id,
+        membershipType: r.membership_type,
+        bungieGlobalDisplayName: r.bungie_global_display_name,
+        bungieGlobalDisplayNameCode: r.bungie_global_display_name_code,
+        lastSeen: r.last_seen,
+        displayName: r.display_name,
+        iconPath: r.icon_path,
+        clears: r.clears
+    }))
 }
