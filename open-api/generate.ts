297a564a36fdc71dea67f250d1efb964c4969022
owensimpson <owensimpson1127@gmail.com>
2024-05-29 16:57:02 -0400

    bump api version

--- open-api/generate.ts
+++ open-api/generate.ts
@@ -16,7 +16,7 @@ const doc = new OpenApiGeneratorV3(registry.definitions).generateDocument({
     info: {
         title: "RaidHub API",
         description: "The Semi-public API for RaidHub",
-        version: "0.1.0",
+        version: "1.0.0",
         contact: {
             name: "RaidHub Admin",
             email: "admin@raidhub.io"
