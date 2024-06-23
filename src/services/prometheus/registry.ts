4ef1c2ded01a32f1509a20ec02f41baff5452deb
owensimpson <owensimpson1127@gmail.com>
2024-06-23 12:48:55 -0400

    add metric

--- src/services/prometheus/registry.ts
+++ src/services/prometheus/registry.ts
@@ -1,6 +1,8 @@
 import { Registry } from "prom-client"
-import { httpRequestTimer } from "./metrics"
+import { activityHistoryQueryTimer, httpRequestTimer, playerProfileQueryTimer } from "./metrics"
 
 export const prometheusRegistry = new Registry()
 
 prometheusRegistry.registerMetric(httpRequestTimer)
+prometheusRegistry.registerMetric(activityHistoryQueryTimer)
+prometheusRegistry.registerMetric(playerProfileQueryTimer)
