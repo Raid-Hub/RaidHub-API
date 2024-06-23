3b53ea663c06f80903f8282c8a58fe1fb4e6ecdc
owensimpson <owensimpson1127@gmail.com>
2024-06-23 15:30:56 -0400

    format

--- src/data-access-layer/player-search.ts
+++ src/data-access-layer/player-search.ts
@@ -26,20 +26,20 @@ export async function searchForPlayer(
         () =>
             postgres.queryRows<PlayerInfo>(
                 `SELECT 
-            membership_id::text AS "membershipId",
-            membership_type AS "membershipType",
-            icon_path AS "iconPath",
-            display_name AS "displayName",
-            bungie_global_display_name AS "bungieGlobalDisplayName",
-            bungie_global_display_name_code AS "bungieGlobalDisplayNameCode",
-            last_seen AS "lastSeen",
-            is_private AS "isPrivate"
-        FROM player 
-        WHERE lower(${opts.global ? "bungie_name" : "display_name"}) LIKE $1 
-            ${opts.membershipType ? "AND membership_type = $3" : ""}
-            AND last_seen > TIMESTAMP 'epoch'
-        ORDER BY _search_score DESC 
-        LIMIT $2;`,
+                    membership_id::text AS "membershipId",
+                    membership_type AS "membershipType",
+                    icon_path AS "iconPath",
+                    display_name AS "displayName",
+                    bungie_global_display_name AS "bungieGlobalDisplayName",
+                    bungie_global_display_name_code AS "bungieGlobalDisplayNameCode",
+                    last_seen AS "lastSeen",
+                    is_private AS "isPrivate"
+                FROM player 
+                WHERE lower(${opts.global ? "bungie_name" : "display_name"}) LIKE $1 
+                    ${opts.membershipType ? "AND membership_type = $3" : ""}
+                    AND last_seen > TIMESTAMP 'epoch'
+                ORDER BY _search_score DESC 
+                LIMIT $2;`,
                 {
                     params: opts.membershipType
                         ? [searchTerm + "%", opts.count, opts.membershipType]
