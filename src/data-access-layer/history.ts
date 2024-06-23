0ba9f037c2d05552c51e1a247d9be87cd5dc64ed
Owen <98496129+owens1127@users.noreply.github.com>
2024-06-23 12:53:28 -0400

    Add query timers (#57)
    * add timers
    
    * move code over
    
    * typo
    
    * add metric

--- src/data-access-layer/history.ts
+++ src/data-access-layer/history.ts
@@ -1,5 +1,7 @@
 import { InstanceForPlayer } from "../schema/components/InstanceForPlayer"
 import { postgres } from "../services/postgres"
+import { activityHistoryQueryTimer } from "../services/prometheus/metrics"
+import { withTimer } from "../services/prometheus/util"
 
 export const getActivities = async (
     membershipId: bigint | string,
@@ -13,42 +15,53 @@ export const getActivities = async (
         cursor?: Date
     }
 ) => {
-    return await postgres.queryRows<InstanceForPlayer>(
-        `SELECT 
-            instance_id::text AS "instanceId",
-            hash::text AS "hash",
-            activity_id AS "activityId",
-            version_id AS "versionId",
-            activity.completed AS "completed",
-            player_count AS "playerCount",
-            score AS "score",
-            fresh AS "fresh",
-            flawless AS "flawless",
-            date_started AS "dateStarted",
-            date_completed AS "dateCompleted",
-            duration AS "duration",
-            platform_type AS "platformType",
-            date_completed < COALESCE(day_one_end, TIMESTAMP 'epoch') AS "isDayOne",
-            date_completed < COALESCE(contest_end, TIMESTAMP 'epoch') AS "isContest",
-            date_completed < COALESCE(week_one_end, TIMESTAMP 'epoch') AS "isWeekOne",
-            JSONB_BUILD_OBJECT(
-                'completed', activity_player.completed,
-                'sherpas', activity_player.sherpas,
-                'isFirstClear', activity_player.is_first_clear,
-                'timePlayedSeconds', activity_player.time_played_seconds
-            ) as player
-        FROM activity_player
-        INNER JOIN activity USING (instance_id)
-        INNER JOIN activity_hash USING (hash)
-        INNER JOIN activity_definition ON activity_definition.id = activity_hash.activity_id
-        WHERE membership_id = $1::bigint
-            AND date_completed < $2
-            ${cutoff ? "AND date_completed > $4" : ""}
-        ORDER BY date_completed DESC
-        LIMIT $3;`,
+    return await withTimer(
+        activityHistoryQueryTimer,
         {
-            params: cutoff ? [membershipId, cursor, count, cutoff] : [membershipId, cursor, count],
-            fetchCount: count
-        }
+            count: count,
+            cursor: String(cursor.getTime() !== 0),
+            cutoff: String(!!cutoff)
+        },
+        () =>
+            postgres.queryRows<InstanceForPlayer>(
+                `SELECT 
+                    instance_id::text AS "instanceId",
+                    hash::text AS "hash",
+                    activity_id AS "activityId",
+                    version_id AS "versionId",
+                    activity.completed AS "completed",
+                    player_count AS "playerCount",
+                    score AS "score",
+                    fresh AS "fresh",
+                    flawless AS "flawless",
+                    date_started AS "dateStarted",
+                    date_completed AS "dateCompleted",
+                    duration AS "duration",
+                    platform_type AS "platformType",
+                    date_completed < COALESCE(day_one_end, TIMESTAMP 'epoch') AS "isDayOne",
+                    date_completed < COALESCE(contest_end, TIMESTAMP 'epoch') AS "isContest",
+                    date_completed < COALESCE(week_one_end, TIMESTAMP 'epoch') AS "isWeekOne",
+                    JSONB_BUILD_OBJECT(
+                        'completed', activity_player.completed,
+                        'sherpas', activity_player.sherpas,
+                        'isFirstClear', activity_player.is_first_clear,
+                        'timePlayedSeconds', activity_player.time_played_seconds
+                    ) as player
+                FROM activity_player
+                INNER JOIN activity USING (instance_id)
+                INNER JOIN activity_hash USING (hash)
+                INNER JOIN activity_definition ON activity_definition.id = activity_hash.activity_id
+                WHERE membership_id = $1::bigint
+                    AND date_completed < $2
+                    ${cutoff ? "AND date_completed > $4" : ""}
+                ORDER BY date_completed DESC
+                LIMIT $3;`,
+                {
+                    params: cutoff
+                        ? [membershipId, cursor, count, cutoff]
+                        : [membershipId, cursor, count],
+                    fetchCount: count
+                }
+            )
     )
 }
